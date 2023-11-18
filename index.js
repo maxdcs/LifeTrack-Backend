c80edaa284d153989f82d6c57bd4afba15ff9d58
maxdcs <maks.drzy@gmail.com>
2023-11-18 19:44:55 +0100

    UI built with backends deploy:full script

--- index.js
+++ index.js
@@ -45,18 +45,62 @@ app.get('/api/exercises/', (request, response) => {
 
 // Get exercise by id
 app.get('/api/exercises/:id', (request, response) => {
-  Exercise.findById(request.params.id).then(result => {
-    response.json(result)
+
+  Exercise.findById(request.params.id)
+    .then(result => {
+      if (result){
+        response.json(result)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/exercises/:id', (request, response, next) => {
+  
+  const body = request.body
+
+  const exercise = {
+    name: body.name,
+    muscleGroup: body.muscleGroup,
+    typeOfReps: body.typeOfReps
+  }
+
+  Exercise.findByIdAndUpdate(request.params.id, exercise, {new: true})
+  .then(updatedExercise => {
+    response.json(updatedExercise)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/foods/:id', (request, response) => {
-  const id = request.params.id
-  foods = foods.filter(f => f.id != id)
-  response.status(204).end()
+app.delete('/api/exercises/:id', (request, response) => {
+  Exercise.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT
+const unknownEndpoint = (request, response) => {
+  response.status(404).json({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' })
+  }
+
+  next(error)
+
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5174
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
